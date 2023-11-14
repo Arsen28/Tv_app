3c2a981837399aecd51f599ebe7d6afc18b111fc
Arsen <arsen281296@gmail.com>
2023-11-14 13:50:04 +0400

    result

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import NavBarItem from './components/SideBar/SideBarItem';
+import SideBar from './components/SideBar';
+import Main from './components/Main';
+// import 
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
